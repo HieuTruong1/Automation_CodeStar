4e934fb6618ad72090c44b29bb34bb07fe5d0774
HieuTruong1 <hieuvan23898@gmail.com>
2024-03-09 22:04:48 +0700

    (09/03/2024) remove auto to update

--- Auto_test/src/main/java/auto/constant/Day12URL.java
+++
@@ -1,6 +0,0 @@
-package auto.constant;
-
-public class Day12URL {
-	public static String URLradio = "https://demo.seleniumeasy.com/basic-radiobutton-demo.html";
-	public static String URLchkbox = "http://demo.seleniumeasy.com/basic-checkbox-demo.html";
-}
--- Auto_test/src/main/java/auto/pages/ERR_MESSAGE.java
+++
@@ -1,9 +0,0 @@
-package auto.pages;
-
-import org.openqa.selenium.By;
-
-public class ERR_MESSAGE {
-	public static By MSG_ERROR_IVALID_EMAIL = By.xpath("//span[text() = 'Please enter a valid email address.']");
-	public static By MSG_AUTHENTICATION_FAILED = By.xpath("//div[normalize-space() = 'Authentication failed!']");
-	public static By MSG_ERROR_REQUIRED = By.xpath("//span[text() = 'This field is required.']");
-}
